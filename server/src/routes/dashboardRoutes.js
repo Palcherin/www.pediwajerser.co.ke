80b27c705189bebd31e7702b868859b7c1fbc1b5
Jobwafula <jobwafulabg@gmail.com>
2026-07-02 08:57:03 +0300

    file structure

--- server/src/routes/dashboardRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getDashboardStats } from '../controllers/dashboardControllers.js';
-import { protect, adminOnly } from '../middleware/authMiddleware.js';
-
-const router = express.Router();
-
-router.get('/stats', protect, adminOnly, getDashboardStats);
-
-export default router;
